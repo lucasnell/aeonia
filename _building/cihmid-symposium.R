19e566cdb5a1345a95742c91044f662819babec6
Lucas Nell <lucnell@gmail.com>
2025-10-07 12:11:26 -0400

    Name change to .Rprofile variable

--- _building/cihmid-symposium.R
+++ _building/cihmid-symposium.R
@@ -37,7 +37,7 @@ handlers("progress")
 
 #' This only gets run if my local .Rprofile has been run and if it's an
 #' interactive session:
-if (interactive() && exists("LAN_USER")) {
+if (interactive() && exists(".LAN_USER")) {
     setHook(packageEvent("grDevices", "onLoad"),
             function(...) grDevices::quartz.options(width = 4, height = 4,
                                                     pointsize = 10))
