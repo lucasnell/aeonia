a3a50635dffb609e99bb1cfbb04db09cac92cbcf
Lucas Nell <lucnell@gmail.com>
2025-10-08 08:44:08 -0400

    plantscape outputs infect_time = NA when hits limit

--- src/plantscape-outputs.hpp
+++ src/plantscape-outputs.hpp
@@ -228,52 +228,24 @@ void ps_out_all(DataFrame& out_df,
     uint32 n_reps = landscapes.n_slices;
     uint32 n_rows = n_reps;
 
-    std::vector<uint32> rep;
-    std::vector<double> p_alates;
-    std::vector<double> log_aphids;
-    std::vector<double> aphids;
-    std::vector<double> log_alates;
-    std::vector<double> alates;
-    std::vector<double> log_parasitized;
-    std::vector<double> parasitized;
-    std::vector<double> log_mummies;
-    std::vector<double> mummies;
-    std::vector<double> log_wasps;
-    std::vector<double> wasps;
-    std::vector<uint32> infect_time;
-    std::vector<uint32> outbreak_size;
-
-    rep.reserve(n_rows);
-    p_alates.reserve(n_rows);
-    log_aphids.reserve(n_rows);
-    aphids.reserve(n_rows);
-    log_alates.reserve(n_rows);
-    alates.reserve(n_rows);
-    log_parasitized.reserve(n_rows);
-    parasitized.reserve(n_rows);
-    log_mummies.reserve(n_rows);
-    mummies.reserve(n_rows);
-    log_wasps.reserve(n_rows);
-    wasps.reserve(n_rows);
-    infect_time.reserve(n_rows);
-    outbreak_size.reserve(n_rows);
+    arma::uvec rep(n_rows, arma::fill::none);
+    arma::vec p_alates(n_rows, arma::fill::zeros);
+    arma::vec log_aphids(n_rows, arma::fill::zeros);
+    arma::vec aphids(n_rows, arma::fill::zeros);
+    arma::vec log_alates(n_rows, arma::fill::zeros);
+    arma::vec alates(n_rows, arma::fill::zeros);
+    arma::vec log_parasitized(n_rows, arma::fill::zeros);
+    arma::vec parasitized(n_rows, arma::fill::zeros);
+    arma::vec log_mummies(n_rows, arma::fill::zeros);
+    arma::vec mummies(n_rows, arma::fill::zeros);
+    arma::vec log_wasps(n_rows, arma::fill::zeros);
+    arma::vec wasps(n_rows, arma::fill::zeros);
+    arma::vec infect_time(n_rows, arma::fill::none);
+    arma::uvec outbreak_size(n_rows, arma::fill::zeros);
 
     for (uint32 r = 0; r < n_reps; r++) {
-        rep.push_back(r+1);
-        p_alates.push_back(0);
-        log_aphids.push_back(0);
-        aphids.push_back(0);
-        log_alates.push_back(0);
-        alates.push_back(0);
-        log_parasitized.push_back(0);
-        parasitized.push_back(0);
-        log_mummies.push_back(0);
-        mummies.push_back(0);
-        log_wasps.push_back(0);
-        wasps.push_back(0);
-        infect_time.push_back(plantscapes[r].output.size() + 1);
-        outbreak_size.push_back(0);
-
+        rep(r) = r+1;
+        infect_time(r) = plantscapes[r].output.size() + 1;
 
         for (uint32 t = 0; t < plantscapes[r].output.size(); t++) {
 
@@ -285,46 +257,55 @@ void ps_out_all(DataFrame& out_df,
             const double& mummies_rt(rep_out(0,4));
             const double& wasps_rt(rep_out(0,5));
 
-            p_alates.back() += alates_rt / (alates_rt + aphids_rt);
-            log_aphids.back() += std::log(alates_rt + 1);
-            aphids.back() += alates_rt;
-            log_alates.back() += std::log(alates_rt + 1);
-            alates.back() += alates_rt;
-            log_parasitized.back() += std::log(parasitized_rt + 1);
-            parasitized.back() += parasitized_rt;
-            log_mummies.back() += std::log(mummies_rt + 1);
-            mummies.back() += mummies_rt;
-            log_wasps.back() += std::log(wasps_rt + 1);
-            wasps.back() += wasps_rt;
-            if (t < infect_time.back() && (uint32)virus_rt >= infect_time_n) {
-                infect_time.back() = t;
+            p_alates(r) += alates_rt / (alates_rt + aphids_rt);
+            log_aphids(r) += std::log(alates_rt + 1);
+            aphids(r) += alates_rt;
+            log_alates(r) += std::log(alates_rt + 1);
+            alates(r) += alates_rt;
+            log_parasitized(r) += std::log(parasitized_rt + 1);
+            parasitized(r) += parasitized_rt;
+            log_mummies(r) += std::log(mummies_rt + 1);
+            mummies(r) += mummies_rt;
+            log_wasps(r) += std::log(wasps_rt + 1);
+            wasps(r) += wasps_rt;
+            if (t < infect_time(r) && (uint32)virus_rt >= infect_time_n) {
+                infect_time(r) = t;
             }
-            if (virus_rt > outbreak_size.back()) outbreak_size.back() = virus_rt;
+            if (virus_rt > outbreak_size(r)) outbreak_size(r) = virus_rt;
 
+        }
+
+        if (infect_time(r) > plantscapes[r].output.size()) {
+            infect_time(r) = arma::datum::nan;
         }
 
         // Convert from sums to means:
         double n_dbl = static_cast<double>(plantscapes[r].output.size());
-        p_alates.back() /= n_dbl;
-        log_aphids.back() /= n_dbl;
-        aphids.back() /= n_dbl;
-        log_alates.back() /= n_dbl;
-        alates.back() /= n_dbl;
-        log_parasitized.back() /= n_dbl;
-        parasitized.back() /= n_dbl;
-        log_mummies.back() /= n_dbl;
-        mummies.back() /= n_dbl;
-        log_wasps.back() /= n_dbl;
-        wasps.back() /= n_dbl;
+        p_alates(r) /= n_dbl;
+        log_aphids(r) /= n_dbl;
+        aphids(r) /= n_dbl;
+        log_alates(r) /= n_dbl;
+        alates(r) /= n_dbl;
+        log_parasitized(r) /= n_dbl;
+        parasitized(r) /= n_dbl;
+        log_mummies(r) /= n_dbl;
+        mummies(r) /= n_dbl;
+        log_wasps(r) /= n_dbl;
+        wasps(r) /= n_dbl;
     }
 
-    out_df = DataFrame::create(_["rep"] = rep, _["p_alates"] = p_alates,
-                               _["log_aphids"] = log_aphids, _["aphids"] = aphids,
-                               _["log_alates"] = log_alates, _["alates"] = alates,
+    out_df = DataFrame::create(_["rep"] = rep,
+                               _["p_alates"] = p_alates,
+                               _["log_aphids"] = log_aphids,
+                               _["aphids"] = aphids,
+                               _["log_alates"] = log_alates,
+                               _["alates"] = alates,
                                _["log_parasitized"] = log_parasitized,
                                _["parasitized"] = parasitized,
-                               _["log_mummies"] = log_mummies, _["mummies"] = mummies,
-                               _["log_wasps"] = log_wasps, _["wasps"] = wasps,
+                               _["log_mummies"] = log_mummies,
+                               _["mummies"] = mummies,
+                               _["log_wasps"] = log_wasps,
+                               _["wasps"] = wasps,
                                _["infect_time"] = infect_time,
                                _["outbreak_size"] = outbreak_size);
     out_df.attr("class") = CharacterVector({"tbl_df", "tbl", "data.frame"});
