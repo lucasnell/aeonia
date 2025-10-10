464df5218ac27b3a10b920c928c31de02c5447eb
Lucas Nell <lucnell@gmail.com>
2025-10-10 13:00:51 -0400

    Added save_plot function

---
+++ R/utils.R
@@ -0,0 +1,62 @@
+#' Save a plot to a PDF file using `cairo_pdf` or `svglite`
+#'
+#' @param filename Filename of plot. Extension should be `".pdf"`, `".svg"`, or `".png"`
+#'     and will determine what type of plot is created.
+#' @param plot Plot object or function to create plot
+#' @param width Width in inches
+#' @param height Height in inches
+#' @param seed Integer to seed RNG for consistent jittering.
+#' @param dpi Resolution to use if creating a png file.
+#' @param fun_args List containing arguments to `plot` if it's a function and has
+#'     required arguments.
+#' @param ... Other arguments to `cairo_pdf` or `svglite`.
+#'
+#' @importFrom svglite svglite
+#'
+#' @return `NULL`
+#' @export
+#'
+save_plot <- function(filename,
+                      plot,
+                      width,
+                      height,
+                      seed = NULL,
+                      dpi = 300,
+                      fun_args = list(),
+                      ...) {
+    stopifnot(is.list(fun_args))
+    ext <- tail(strsplit(filename, "\\.")[[1]], 1)
+    if (!ext %in% c("pdf", "svg", "png")) {
+        stop("ERROR: file name extension must be \".pdf\", \".svg\", or \".png\"")
+    }
+    fn_dir <- dirname(filename)
+    if (!dir.exists(fn_dir)) stop("ERROR: `", fn_dir, "` doesn't exist")
+    if (!is.null(seed)) set.seed(seed)
+    if (ext == "pdf") {
+        cairo_pdf(filename = filename, width = width, height = height, ...)
+        if (is.function(plot)) {
+            do.call(plot, fun_args)
+        } else {
+            plot(plot)
+        }
+        dev.off()
+    } else if (ext == "svg") {
+        svglite(filename = filename, width = width, height = height, ...)
+        if (is.function(plot)) {
+            do.call(plot, fun_args)
+        } else {
+            plot(plot)
+        }
+        dev.off()
+    } else {
+        png(filename = filename, width = width, height = height,
+            units = "in", res = dpi, ...)
+        if (is.function(plot)) {
+            do.call(plot, fun_args)
+        } else {
+            plot(plot)
+        }
+        dev.off()
+    }
+    invisible(NULL)
+}
