4272859227dd8c425c4485624bfaa2f43feb3b96
Lucas Nell <lucnell@gmail.com>
2025-10-15 16:24:32 -0400

    Not using B in insect pop comparison

--- _scripts/00-host-parasitoid-model/_insect-pop-comparison.R
+++ _scripts/00-host-parasitoid-model/_insect-pop-comparison.R
@@ -55,7 +55,7 @@ run_goc <- function(aphids0, wasps0, max_t = 100) {
 
 
 run_aeonia <- function(aphids0, wasps0, max_t = 100) {
-    test_insect_pops(max_t = max_t, N0 = aphids0, W0 = 0, Y0 = wasps0, B = 0,
+    test_insect_pops(max_t = max_t, N0 = aphids0, W0 = 0, Y0 = wasps0,
                      alate_0 = -Inf, alate_1 = 0) |>
         mutate(aphids = aphids + alates) |>
         select(time, aphids, wasps) |>
